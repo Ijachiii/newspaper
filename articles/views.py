de04d1a654133631e67c73552dbabfa189edf689
Dave <davidaudu1010@gmail.com>
2023-01-05 21:33:26 +0100

    Created Articles urls.py and views

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import *
+from .models import *
 
 # Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = "article_list.html"
+    context_object_name = "article_list"
