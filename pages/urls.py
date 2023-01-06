02c396d64c32e4cc7ab95749129b73152b5f3abf
Dave <davidaudu1010@gmail.com>
2023-01-06 11:41:11 +0100

    updated aricles.urls

--- pages/urls.py
+++ pages/urls.py
@@ -2,8 +2,5 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path("", ArticleLiatView.as_view(), name="article_list"),
-    path("<int:pk>/edit/", ArticleUpdateview.as_view(), name="article_edit"),
-    path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
-    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete")
+    path("", HomePageView.as_view(), name="home"),
 ]
